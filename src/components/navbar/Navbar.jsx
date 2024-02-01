e678a1a7e8e9cc91eaa9beca8bfe3bd3ee2ae10d
zihadhossion <zihadhossion2005@gmail.com>
2024-02-01 16:51:17 +0600

    header navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./navbar.scss";
 import SearchIcon from '@mui/icons-material/Search';
 import FullscreenIcon from '@mui/icons-material/Fullscreen';
@@ -5,59 +6,69 @@ import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import flag from "../../assets/flag.png";
 import profile from "../../assets/profile.jpg";
 
 function Navbar() {
-    return (
-        <nav className="navbar">
-            <div className="header-country">
-                <button className="flag">
-                    <img src={flag} alt="" />
-                </button>
-            </div>
-            <div className="header-search">
-                <button className="search">
-                    <SearchIcon />
-                </button>
-            </div>
-            <div className="header-darkmode">
-                <button className="darkmode">
-                    <DarkModeOutlinedIcon />
-                </button>
-            </div>
-            <div className="header-fullscreen">
-                <button className="fullscreen">
-                    <FullscreenIcon />
-                </button>
-            </div>
-            <div className="header-shopcart">
-                <button className="shopcart">
-                    <ShoppingBasketIcon className="icon" />
-                    <p className="counter red">4</p>
-                </button>
-            </div>
-            <div className="header-notification">
-                <button className="notification">
-                    <NotificationsNoneOutlinedIcon className="icon" />
-                    <span className="outer">
-                        <span className="animate-ping"></span>
-                        <span className="counter green">3</span>
-                    </span>
+    const [sidebarActive, setSidebarActive] = useState(true);
 
-                </button>
-            </div>
-            <div className="header-bookmark">
-                <button className="bookmark">
-                    <BookmarkBorderOutlinedIcon />
-                </button>
-            </div>
-            <div className="header-user">
-                <button className="profile">
-                    <img src={profile} alt="" />
-                </button>
-            </div>
-        </nav>
+    return (
+        <header id="header">
+            <nav className="navbar">
+                <div className="left">
+                    {sidebarActive ? <ArrowForwardIcon /> : <ArrowBackIcon />}
+                </div>
+                <div className="right">
+                    <div className="header-country">
+                        <button className="flag">
+                            <img src={flag} alt="" />
+                        </button>
+                    </div>
+                    <div className="header-search">
+                        <button className="search">
+                            <SearchIcon />
+                        </button>
+                    </div>
+                    <div className="header-darkmode">
+                        <button className="darkmode">
+                            <DarkModeOutlinedIcon />
+                        </button>
+                    </div>
+                    <div className="header-fullscreen">
+                        <button className="fullscreen">
+                            <FullscreenIcon />
+                        </button>
+                    </div>
+                    <div className="header-shopcart">
+                        <button className="shopcart">
+                            <ShoppingBasketIcon className="icon" />
+                            <p className="counter red">4</p>
+                        </button>
+                    </div>
+                    <div className="header-notification">
+                        <button className="notification">
+                            <NotificationsNoneOutlinedIcon className="icon" />
+                            <span className="outer">
+                                <span className="animate-ping"></span>
+                                <span className="counter green">3</span>
+                            </span>
+                        </button>
+                    </div>
+                    <div className="header-bookmark">
+                        <button className="bookmark">
+                            <BookmarkBorderOutlinedIcon />
+                        </button>
+                    </div>
+                    <div className="header-user">
+                        <button className="profile">
+                            <img src={profile} alt="" />
+                        </button>
+                    </div>
+                </div>
+            </nav>
+        </header>
     )
 };
 
